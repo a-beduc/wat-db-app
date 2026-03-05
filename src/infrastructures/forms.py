8838a91ab9065bbaf10b0924f76220bb325a0b0b
abeduc <alexandre.beduc@gmail.com>
2026-03-05 17:34:35 +0100

    add infrastructure forms

--- src/infrastructures/forms.py
+++ src/infrastructures/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-class WaterMeterCreateForm(forms.Form):
+class WaterMeterForm(forms.Form):
     rg_code = forms.CharField(max_length=10)
     internal_number = forms.IntegerField(min_value=0)
     serial_id = forms.CharField(max_length=15)
@@ -13,3 +13,13 @@ class WaterMeterCreateForm(forms.Form):
 
     def clean_serial_id(self):
         return self.cleaned_data['serial_id'].strip().upper()
+
+
+def water_meter_initial(water_meter):
+    return {
+        'rg_code': water_meter.rg_code,
+        'internal_number': water_meter.internal_number,
+        'serial_id': water_meter.serial_id,
+        'subscriber_name': water_meter.subscriber_name,
+        'raw_address': water_meter.raw_address,
+    }
