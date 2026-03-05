a330b28bfa05b87dbbd42e712cbe10b1aba0352d
abeduc <alexandre.beduc@gmail.com>
2026-03-05 17:35:28 +0100

    add views url infrastructures

--- src/infrastructures/urls.py
+++ src/infrastructures/urls.py
@@ -5,6 +5,9 @@ from infrastructures.views import (
     water_meter_create,
     water_meter_list,
     water_meter_detail,
+    water_meter_edit,
+    water_meter_update,
+    water_meter_delete,
 )
 
 
@@ -16,4 +19,7 @@ urlpatterns = [
     path('create/', water_meter_create, name='create'),
     path('list/', water_meter_list, name='list'),
     path('detail/<int:pk>/', water_meter_detail, name='detail'),
+    path('edit/<int:pk>/', water_meter_edit, name='edit'),
+    path('update/<int:pk>/', water_meter_update, name='update'),
+    path('delete/<int:pk>/', water_meter_delete, name='delete'),
 ]
--- src/infrastructures/views.py
+++ src/infrastructures/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import redirect
 from django.views.decorators.http import require_GET, require_POST
 from django.urls import reverse
 
-from infrastructures.forms import WaterMeterCreateForm
-from infrastructures.services import create_water_meter
+from infrastructures.forms import WaterMeterForm, water_meter_initial
+from infrastructures.services import (
+    create_water_meter,
+    update_water_meter,
+    delete_water_meter,
+)
 from infrastructures.selectors import (
     water_meter_list_all,
     water_meter_get_by_pk,
@@ -13,7 +17,7 @@ from infrastructures.selectors import (
 
 @require_GET
 def water_meter_new(request):
-    form = WaterMeterCreateForm()
+    form = WaterMeterForm()
     return TemplateResponse(
         request,
         'infrastructures/water_meter_new.html',
@@ -23,7 +27,7 @@ def water_meter_new(request):
 
 @require_POST
 def water_meter_create(request):
-    form = WaterMeterCreateForm(request.POST)
+    form = WaterMeterForm(request.POST)
     if not form.is_valid():
         return TemplateResponse(
             request,
@@ -55,3 +59,49 @@ def water_meter_detail(request, pk):
         'infrastructures/water_meter_detail.html',
         {'water_meter': water_meter},
     )
+
+
+@require_GET
+def water_meter_edit(request, pk):
+    water_meter = water_meter_get_by_pk(pk)
+
+    form = WaterMeterForm(initial=water_meter_initial(water_meter))
+
+    return TemplateResponse(
+        request,
+        'infrastructures/water_meter_edit.html',
+        {
+            'form': form,
+            'water_meter': water_meter,
+        },
+    )
+
+
+@require_POST
+def water_meter_update(request, pk):
+    water_meter = water_meter_get_by_pk(pk)
+
+    form = WaterMeterForm(request.POST)
+
+    if not form.is_valid():
+        return TemplateResponse(
+            request,
+            'infrastructures/water_meter_edit.html',
+            {
+                'form': form,
+                'water_meter': water_meter,
+            },
+        )
+
+    update_water_meter(water_meter=water_meter, data=form.cleaned_data)
+    return redirect(
+        reverse('infrastructures:detail', kwargs={'pk': water_meter.pk})
+    )
+
+
+@require_POST
+def water_meter_delete(request, pk):
+    water_meter = water_meter_get_by_pk(pk)
+
+    delete_water_meter(water_meter=water_meter)
+    return redirect(reverse('infrastructures:list'))
