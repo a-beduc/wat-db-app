5ba0c37f7cbaf8cad86e2e573608a12cbd1f0cdb
abeduc <alexandre.beduc@gmail.com>
2026-03-07 00:06:34 +0100

    misc: -.env.example -simplify src/config/env.py -fixed lint error (unused import)

--- src/config/env.py
+++ src/config/env.py
@@ -7,7 +7,7 @@ env = environ.Env()
 ROOT_DIR = Path(__file__).resolve().parents[2]
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-ENV_FILE = ROOT_DIR / ".env"
+ENV_FILE = ROOT_DIR / '.env'
 
 if ENV_FILE.exists():
-    environ.Env.read_env(os.path.join(ROOT_DIR, ".env"))
+    environ.Env.read_env(ENV_FILE)
