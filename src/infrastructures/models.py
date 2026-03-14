e5039621e5a829c339bd970f517d9786ae998749
abeduc <alexandre.beduc@gmail.com>
2026-03-14 18:22:18 +0100

    add Reading model (#6)

--- src/infrastructures/models.py
+++ src/infrastructures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class WaterMeter(models.Model):
@@ -7,3 +8,25 @@ class WaterMeter(models.Model):
     serial_id = models.CharField(max_length=15)
     subscriber_name = models.CharField(max_length=200)
     raw_address = models.CharField(max_length=200)
+
+
+class Reading(models.Model):
+    reading_date = models.DateField()
+    value_m3 = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    water_meter = models.ForeignKey(
+        WaterMeter, on_delete=models.CASCADE, related_name='readings'
+    )
+
+    def save(self, *args, **kwargs):
+        """
+        Block update of reading once created (immutable entry).
+        May be bypassed by QueryUpdate (see django-immutablemodel if relevant later)
+
+        source: https://stackoverflow.com/questions/5236627/can-i-make-a-django-model-object-immutable
+        """
+        if self.pk:
+            raise ValidationError(
+                f'You may not edit an existing {self._meta.model_name}'
+            )
+        super(Reading, self).save(*args, **kwargs)
---
+++ tests/test_infrastructures/test_models.py
@@ -0,0 +1,55 @@
+import pytest
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+
+from infrastructures.models import Reading, WaterMeter
+
+
+@pytest.fixture
+def water_meter():
+    return WaterMeter.objects.create(
+        rg_code='RG01',
+        internal_number=1,
+        serial_id='A00AA000000A',
+        subscriber_name='John Doe',
+        raw_address='123 lambda street',
+    )
+
+
+@pytest.mark.django_db
+def test_create_reading_with_water_meter_works(water_meter):
+    reading = Reading.objects.create(
+        reading_date='2026-03-14',
+        value_m3=100,
+        water_meter=water_meter,
+    )
+
+    assert reading.pk is not None
+    assert reading.water_meter == water_meter
+    assert reading.value_m3 == 100
+
+
+@pytest.mark.django_db
+def test_create_reading_without_water_meter_fails():
+    with pytest.raises(IntegrityError):
+        Reading.objects.create(
+            reading_date='2026-03-14',
+            value_m3=100,
+        )
+
+
+@pytest.mark.django_db
+def test_update_reading_is_blocked(water_meter):
+    reading = Reading.objects.create(
+        reading_date='2026-03-14',
+        value_m3=100,
+        water_meter=water_meter,
+    )
+
+    reading.value_m3 = 200
+
+    with pytest.raises(
+        ValidationError,
+        match=f'You may not edit an existing {Reading._meta.model_name}',
+    ):
+        reading.save()
